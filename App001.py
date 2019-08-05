80a1ffb2ac8cce10a572608e6a148818433ea26d
Siavash Ebrahimi <ebrahimi.siavash.com>
2019-08-05 15:13:33 +0430

    Satarting to code Random Country with its same greeting.

--- App001.py
+++ App001.py
@@ -2,9 +2,16 @@ import os
 import sys
 import random
 
+#-- Adding Dictionary --------------------
+Ra_Country = {"Iran":"سلام", "Spain":"Holla", "England":"Hi"}
+#------------------------------------------------------
+
 Greeting = ["Hi", "Holla", "Hello", "Welcome", "Salam",
             "Marhaba", "Saboolik", "سلام"]
 H = input("What is your name: ")
+#---Branch Code--------------------
+print(Ra_Country["Iran"], H)
+#------------------------------------------------------
 x=0;
 while x != 5:
     print(random.choice(Greeting), H)
