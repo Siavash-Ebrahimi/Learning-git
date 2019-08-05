05b5fcdcd01a4f5c3140aaee38a061b07c727b36
Siavash Ebrahimi <ebrahimi.siavash.com>
2019-08-05 16:07:41 +0430

    Random Contry with its greeting message added

--- App001.py
+++ App001.py
@@ -3,9 +3,18 @@ import sys
 import random
 
 #-- Adding Dictionary --------------------
-Ra_Country = {"Iran":"سلام", "Spain":"Holla", "England":"Hi"}
+H = input("What is your name: ")
+c = input('Where are you from? ')
+Greeting = {"Iran":"سلام", "Spain":"Holla", "England":"Hi"}
+Country = ['Iran', 'Spain', 'England']
+##print(Ra_Country)
+x = 0;
+for i in Country:
+    if i == c:
+        print(Greeting[Country[x]], H, 'From', c)
+    x += 1
 #------------------------------------------------------
-
+'''
 Greeting = ["Hi", "Holla", "Hello", "Welcome", "Salam",
             "Marhaba", "Saboolik", "سلام"]
 H = input("What is your name: ")
@@ -17,3 +26,4 @@ while x != 5:
     print(random.choice(Greeting), H)
     x += 1
 
+'''
